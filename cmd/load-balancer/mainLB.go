798f8d486c34e548421b243fd7b4379502d2f01a
real-Slim-Shardy <andreshrlmv@gmail.com>
2024-04-05 23:17:45 +0300

    add ReadInitCfgFile
    manager.go
    - Add json format structs to
    - Add Instance struct
    - Add and test ReadInitCfgFile()
    
    mainLB.go
    - Add code plan
    - Add Initialization of Logger
    - Add Reading Config from json file

--- cmd/load-balancer/mainLB.go
+++ cmd/load-balancer/mainLB.go
@@ -1,5 +1,37 @@
 package main
 
+import (
+	"ddbms/lib/manager"
+	"fmt"
+)
+
+const (
+	initCfgFileName = "InitConfig.json"
+	filePath        = "../../cfg/"
+)
+
 func main() {
-	
+	// ============ Starting system ============
+	// init logger
+	manager.InitLogger()
+
+	// get config
+	data, err := manager.ReadInitCfgFile(filePath + initCfgFileName)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(data)
+
+	// test system availability
+
+	// ========== Backgroud activity ===========
+	// start heartbeat system
+
+	// getting metrics from instances
+
+	// ========== Work with Requests ===========
+	// init listener
+
+	// requests redirecting
+
 }
--- lib/manager/manager.go
+++ lib/manager/manager.go
@@ -1,1 +1,91 @@
 package manager
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+// Structure that implements unit's data from InitConfig file from user
+type InitCfgUnit struct {
+	Number  int    `json:"number"`
+	Address string `json:"address"`
+}
+
+// Structure that implements All json file data format
+type CfgData struct {
+	Lb      []InitCfgUnit `json:"load-balancers"`
+	Servers []InitCfgUnit `json:"servers"`
+	DBC     []InitCfgUnit `json:"db-clusters"`
+}
+
+// Structure that implements Instance information
+type Instance struct {
+	Type    string
+	Number  int
+	Address string
+	Status  string
+}
+
+// Read InitConfig.json file and transfers raw data to special struct
+// Function read json file with configuration
+// Return a map ["InstanceName"]:"Instance{Type:string; Number: int; Address: string; Status: string}"
+func ReadInitCfgFile(fileName string) (*map[string]Instance, error) {
+	// Configuration file from user stored in cfg/*InitCfgFileName*
+	buf, e := os.ReadFile(fileName)
+	if e != nil {
+		return nil, errors.New("Can't open Init Configuration File or it doesn't exists")
+	}
+
+	// Unmarshal data from json file to variable 'data'
+	var data CfgData
+	json.Unmarshal(buf, &data)
+
+	// init output variable cfg
+	cfg := make(map[string]Instance)
+
+	// Parse data to result format
+	// Parse LB information
+	for i := 0; i < len(data.Lb); i++ {
+		cfg["LB"+strconv.Itoa(i)] = Instance{
+			Type:    "LB",
+			Number:  data.Lb[i].Number,
+			Address: data.Lb[i].Address,
+			Status:  "Ready"}
+	}
+
+	// Parse Servers information
+	for i := 0; i < len(data.Servers); i++ {
+		cfg["Server"+strconv.Itoa(i)] = Instance{
+			Type:    "Server",
+			Number:  data.Servers[i].Number,
+			Address: data.Servers[i].Address,
+			Status:  "Ready"}
+	}
+
+	// Parse DB clusters information
+	for i := 0; i < len(data.DBC); i++ {
+		cfg["DB"+strconv.Itoa(i)] = Instance{
+			Type:    "DB",
+			Number:  data.DBC[i].Number,
+			Address: data.DBC[i].Address,
+			Status:  "Ready"}
+	}
+
+	// ОТЛАДКА
+	fmt.Println(data)
+	return &cfg, nil
+}
+
+// Create or Open file logs for storing all logs in it
+func InitLogger() {
+	file, err := os.OpenFile("logs", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err == nil {
+		log.SetOutput(file)
+	} else {
+		log.Fatal("Failed to log to file, using default stderr")
+	}
+}
